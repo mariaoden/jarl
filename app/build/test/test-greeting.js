fbe426c87a052ca0c522fa45e4c7ab1e12da3807
Maria <mariaoden@users.noreply.github.com>
2015-11-08 23:47:31 +0100

    Projektstart

---
+++ app/build/test/test-greeting.js
@@ -0,0 +1,9 @@
+/// <reference path="../typings/jasmine/jasmine.d.ts" />
+/**
+ * Test App.js
+ */
+describe('typescript greeter', function () {
+    it('should do something', function () {
+        // remove comments
+    });
+});
