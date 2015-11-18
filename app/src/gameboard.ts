e8a2b179cc20c962b8f9d9576700c59e269171a8
Maria <mariaoden@users.noreply.github.com>
2015-11-18 14:12:39 +0100

    started with addTile exit q Q quit

--- app/src/gameboard.ts
+++ app/src/gameboard.ts
@@ -12,6 +12,18 @@ module Jarl {
 			} 
 		}
 		
+		private isBoardEmpty() : boolean {
+			var occupied : boolean[];
+			var isEmpty : boolean;
+			for (var i = 0; i < 6; i++) {
+				for (var j = 0; j < 6; j++) {
+					occupied.push(this.gameboard[i][j].getOccupied());
+				}
+			}
+			isEmpty = (_.findWhere(occupied, true) == undefined);
+			return true;
+		};
+		
 		public getGameboard() : Array<Array<BoardSquareInterface>> {
 			return this.gameboard;
 		}
@@ -25,10 +37,22 @@ module Jarl {
 		}
 		
 		public addTileToGameboard(tile : Tile, row :number, column : number) {
+			// Make sure that the Jarl is the first tile to be added. And it is always black who starts
+			if (tile.tileType == TypeOfTile.Jarl && tile.color == Color.Black) {
+				if ((row == 0 && ( column == 2 || column == 3)) && this.isBoardEmpty()) {
+					this.gameboard[row][column].setBoardSquare(true, tile);
+				} else {
+					throw new NotValidPositionException();
+				}
+			}
 			
 		}
-		
+				
 	};
+	
+	export function NotValidPositionException() {
+		console.log('Not valid position');
+	}
 	
 	export enum Color {White, Black, Undefined};
 	export enum TypeOfTile {Jarl, Freeman, Undefined};
@@ -69,4 +93,5 @@ module Jarl {
 			return this.tile;
 		}
 	};
+
 };
